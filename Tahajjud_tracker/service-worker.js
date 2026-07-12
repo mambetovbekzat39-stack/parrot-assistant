31d127c0c838da71ff4e308f662513637240037b
mambetovbekzat39 <mambetovbekzat39@gmail.com>
2026-07-12 22:04:07 +0600

    Update cachen

--- Tahajjud_tracker/service-worker.js
+++ Tahajjud_tracker/service-worker.js
@@ -1,21 +1,43 @@
-const CACHE_NAME = "tahajjud-v5";
+const CACHE_NAME = "tahajjud-v6";
 
 const urlsToCache = [
   "/tahajjud_tracker/",
   "/tahajjud_tracker/index.html",
-  "/tahajjud_tracker/icon.png"
+  "/tahajjud_tracker/icon.png",
+  "/tahajjud_tracker/manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
