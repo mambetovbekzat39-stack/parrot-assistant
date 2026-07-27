849bc7b4ee12833f78ddfe899039535fe8d9d75b
mambetovbekzat39 <mambetovbekzat39@gmail.com>
2026-07-27 23:49:45 +0600

    обновил приложение деньги 2

--- dolgi/service-worker.js
+++ dolgi/service-worker.js
@@ -1,26 +1,50 @@
 const CACHE_NAME = "dolgi-v1";
 
-const urlsToCache = [
-  "/dolgi/",
-  "/dolgi/index.html",
-  "/dolgi/icon.png"
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./icon.png",
+  "./manifest.json"
 ];
+
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+
   self.skipWaiting();
 });
 
+
 self.addEventListener("activate", event => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+
+        return response || fetch(event.request)
+          .catch(() => caches.match("./index.html"));
+
+      })
   );
+
 });
