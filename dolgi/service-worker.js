7486a14dd5fd1863ac8d80b1ad9c80a0a046c29a
mambetovbekzat39 <mambetovbekzat39@gmail.com>
2026-07-27 23:07:08 +0600

    новый приложение обновил 2

--- dolgi/service-worker.js
+++ dolgi/service-worker.js
@@ -1,22 +1,43 @@
-const CACHE_NAME = "debt-tracker-v1";
+const CACHE_NAME = "financeTracker-v1";
 
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./manifest.json",
-    "./icon.png"
+const urlsToCache = [
+  "/dolgi/",
+  "/dolgi/index.html",
+  "/dolgi/icon.png",
+  "/dolgi/manifest.json"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-    );
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
